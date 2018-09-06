069fba6461da9769a8251534805b7d3e53a84225
Iswarya Ayyappan <iswarya.ayyappan@gmail.com>
2018-09-06 04:39:24 +0000

    added env DATABASEURL

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var indexRoute       = require("./routes/index"),
 
 var app = express();
 //mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
-mongoose.connect("mongodb://iswarya91:isaMAR1991@ds245512.mlab.com:45512/yelpcamp_db", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 //SeedDB();
 
 app.use(bodyParser.urlencoded({ extended: true }));
